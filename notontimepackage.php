b535ba58003f1f7a0faf89ce58f1ab56ba8aa2ce
jackywoo <jackywu0203@gmail.com>
2022-06-20 16:18:08 +0800

    new not on time package page

---
+++ notontimepackage.php
@@ -0,0 +1,31 @@
+<?php
+// db settings
+$hostname = '140.127.74.186';
+$username = '410977002';
+$password = '410977002';
+$database = '410977002';
+
+// db connection
+$con = mysqli_connect($hostname, $username, $password, $database) or die("Error " . mysqli_error($con));
+
+// fetch records
+$sql = "SELECT `package`.`package_id` , `package`.`package_type` ,`customer`.`name` , `time`.`schedule_time` , `time`.`end_time` , `transportation`.`type_name`
+FROM `package` 
+LEFT JOIN `payment` ON `package`.`package_id`=`payment`.`package_id`  
+LEFT JOIN `time` ON `time`.`package_id`=`package`.`package_id` 
+LEFT JOIN `customer` ON `payment`.`customer_id`=`customer`.`customer_id` 
+LEFT JOIN `transportation` ON `transportation`.`transportation_id`=`package`.`transportation_id`
+WHERE `time`.`end_time` > `time`.`schedule_time`";
+$result = mysqli_query($con, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+}
+
+$dataset = array(
+    "echo" => 1,
+    "totalrecords" => count($array),
+    "totaldisplayrecords" => count($array),
+    "data" => $array
+);
+echo json_encode($dataset);
