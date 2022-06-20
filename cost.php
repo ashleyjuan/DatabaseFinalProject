25a19dfdf59a29acb823a6cb9624d75f2e0e6351
jackywoo <jackywu0203@gmail.com>
2022-06-20 15:54:33 +0800

    no message

---
+++ cost.php
@@ -0,0 +1,30 @@
+<?php
+// db settings
+$hostname = '140.127.74.186';
+$username = '410977002';
+$password = '410977002';
+$database = '410977002';
+
+// db connection
+$con = mysqli_connect($hostname, $username, $password, $database) or die("Error " . mysqli_error($con));
+
+// fetch records
+$sql = "SELECT `customer`.`name` , `customer`.`phone_number` , `customer`.`email` , SUM(`payment`.`fee`) AS cost
+FROM `package` 
+LEFT JOIN `payment` ON `package`.`package_id`=`payment`.`package_id`  
+LEFT JOIN `customer` ON `payment`.`customer_id`=`customer`.`customer_id` 
+GROUP BY `payment`.`customer_id`
+ORDER BY cost DESC";
+$result = mysqli_query($con, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+}
+
+$dataset = array(
+    "echo" => 1,
+    "totalrecords" => count($array),
+    "totaldisplayrecords" => count($array),
+    "data" => $array
+);
+echo json_encode($dataset);
